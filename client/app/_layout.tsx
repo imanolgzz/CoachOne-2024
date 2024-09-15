e33815748a056d31d89ce61ea426e425ce47eead
NicoM3Nd0Z4 <a00835649@tec.mx>
2024-09-15 00:32:45 -0600

    Start of investement tab

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,11 +1,13 @@
 import { AuthProvider } from '@/hooks/AuthContext';
 import Page from '@/app/page';
+import HomeScreen from './(tabs)';
+import InvestmentSceen from './(tabs)/investment';
 
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <Page />
+      <InvestmentSceen />
     </AuthProvider>
   );
 }
