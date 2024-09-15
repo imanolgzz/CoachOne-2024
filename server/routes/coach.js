2b1861a4d9460883270783726e05fe2fe31eebe7
Pedro <pedrosava01@gmail.com>
2024-09-15 01:32:12 -0600

    chatbot , basic layout , gpt

---
+++ server/routes/coach.js
@@ -0,0 +1,21 @@
+import OpenAI from "openai"
+import dotenv from 'dotenv'
+dotenv.config();
+
+const openai = new OpenAI({
+    apiKey:OPENAI_API_KEY
+});
+
+async function main(){
+    const myAssistant = await openai.beta.assistants.create({
+        instructions:
+            "You are a personal finance coach, When asked a question about personal finances, or trading, write and run Javascript code to answer the question",
+        name:"Coach One",
+        tools:[{type:"code_interpreter"}],
+        model:"gpt-4o"
+    });
+
+    console.log(myAssistant)
+}
+
+main();
